b3849ec08aff16c4604afd0bfd5576a00b289f7b
Faiz Mohammad Hafidza <faizmohhafidza@gmail.com>
2022-08-23 10:11:11 +0700

    added assets and utils

---
+++ utils/formatDate.js
@@ -0,0 +1,12 @@
+export function formatDate(date) {
+    const d = new Date(date);
+    const dtf = new Intl.DateTimeFormat("en", {
+        year: "numeric",
+        month: "long",
+        day: "2-digit",
+    });
+    const [{ value: month }, , { value: day }, , { value: year }] =
+        dtf.formatToParts(d);
+
+    return `${day} ${month} ${year}`;
+}
